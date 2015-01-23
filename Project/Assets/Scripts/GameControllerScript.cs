823ced3c5763f1333c738332147d9e16b1260f6b
SergeOxe <sergeoxe@gmail.com>
2015-01-23 14:59:37 +0200

    SergeInitialCommit

---
+++ Project/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+	public float keyPressedTime;
+	private bool playerPressed;
+	public float maxTime;
+
+	// Use this for initialization
+	void Start () {
+		keyPressedTime = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			playerPressed = true;
+			keyPressedTime += Time.deltaTime;
+		}
+
+		if(Input.GetMouseButtonUp(0) && playerPressed){
+			print(keyPressedTime);
+			calcLength(keyPressedTime);
+			keyPressedTime = 0;
+			playerPressed= false;
+		}
+	}
+
+	void calcLength (float time){
+		if (time > maxTime) {
+		}
+	}
+}
