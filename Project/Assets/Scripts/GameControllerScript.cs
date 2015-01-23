7f5ccb651df689b211865f2b8cc7a06d2f78a37d
SergeOxe <sergeoxe@gmail.com>
2015-01-23 18:27:58 +0200

    nothing yet

--- Project/Assets/Scripts/GameControllerScript.cs
+++ Project/Assets/Scripts/GameControllerScript.cs
@@ -6,11 +6,14 @@ public class GameControllerScript : MonoBehaviour {
 	private bool playerPressed;
 	public float maxTime;
 	public float growSpeed;
-
+	public float forcePush;
+	public Collider2D circleCollider;
+	Vector2 size;
 	// Use this for initialization
 	void Start () {
 		keyPressedTime = 0;
 	
+		
 	}
 	
 	// Update is called once per frame
@@ -26,13 +29,15 @@ public class GameControllerScript : MonoBehaviour {
 			calcLength(keyPressedTime);
 			keyPressedTime = 0;
 			playerPressed= false;
+			this.rigidbody2D.AddForce((new Vector2(1,0)) *forcePush );
 		}
 	}
 
 	void changeLadderSize ()
 	{
 		float yScale = this.transform.localScale.y +  growSpeed;
-		this.transform.localScale = new Vector3 (1, yScale, 0);;
+		this.transform.localScale = new Vector3 (1, yScale, 0);
+		//this.transform.position = new Vector3 (this.transform.position.x,this.transform.position.y + (growSpeed-1)/2,1);
 	}
 
 	void calcLength (float time){
