051bf3dc67d995692dc4c10b8f666a12a2093951
TalKashi <shakikashi@gmail.com>
2015-01-23 21:07:27 +0200

    commit GUI scenes

---
+++ Project/Assets/MainMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+    private GameEventsScript GameEvents = GameEventsScript.GameEventsInstance;
+
+    public void OnPlayButtonClick()
+    {
+        GameEvents.OnGameRestart();
+        Application.LoadLevel("Game_GUI");
+    }
+}
