ae67c7e4ffb251e5d79d4b1958d536c2ed1101fd
TalKashi <shakikashi@gmail.com>
2015-01-23 17:13:59 +0200

    GUI

---
+++ Project/Assets/Scripts/GameEventsScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+
+public class GameEventsScript {
+
+	public Action increamentScore;
+
+    private static GameEventsScript s_GameEvents;
+
+    private GameEventsScript()
+    {
+        
+    }
+
+    public static GameEventsScript GameEvents
+    {
+        get
+        {
+            if (s_GameEvents == null)
+                s_GameEvents = new GameEventsScript();
+            return s_GameEvents;
+        }
+    }
+
+    public void OnIncreamentScore()
+    {
+        if (increamentScore != null)
+            increamentScore();
+    }
+}
---
+++ Project/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+    public Text ScoreText;
+
+    GameEventsScript GameEvents = GameEventsScript.GameEvents;
+
+    private int score;
+
+    void Start() 
+    {
+        score = 0;
+        GameEvents.increamentScore += ScoreChangedListener;
+        ScoreText.text = score.ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            GameEvents.OnIncreamentScore();
+    }
+
+    void ScoreChangedListener()
+    {
+        score++;
+        ScoreText.text = score.ToString();
+    }
+
+
+}
